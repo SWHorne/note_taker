d7934346dbb8f942888ca17ec6ec4179dd9adef2
Sam Horne <swhorne@gmail.com>
2020-05-16 23:14:01 -0600

    updating apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,35 @@
+let fs = require('fs');
+let util = require('util');
+let store = require('../db/store');
+// const path = require('path');
+let readFileAsync = util.promisify(fs.readFile);
+let writeFileAsync = util.promisify(fs.writeFile);
+
+
+module.exports = function(app) {
+ 
+//get
+  app.get("/api/notes", function(req, res) {
+    store.getNotes().then((data) => {
+        return res.json(data);
+    });    
+});
+
+//POST
+
+  app.post("/api/notes", function(req, res) {
+    store.addNote(req.body).then((note) => {
+        return res.json(note);
+    })
+
+  });
+
+ //Delete
+
+  app.delete("/api/notes/:id", function(req, res) {
+   store.deleteNote(req.params.id).then(() => {
+       return res.json({ok:true});
+   })
+  });
+
+};
