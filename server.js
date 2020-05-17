6795c13e7228dd0e82ac97c6958586adf159609e
Sam Horne <swhorne@gmail.com>
2020-05-16 23:16:53 -0600

    Update server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+//dependencies
+const express = require('express');
+const path = require('path');
+const app = express();
+
+let PORT = process.env.PORT || 3000;
+
+//Sets up express app to handle parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
